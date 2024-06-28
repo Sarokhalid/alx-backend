d1f611a35baea593fb140c285fd1f8e0c1f4aabc
sara <saramustafa1842002@gmail.com>
2024-06-28 21:54:09 +0200

    done

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,20 +8,18 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-        '''A class `BasicCache` that inherits from `BaseCaching`
-               and is a caching system
-                   '''
+    '''A class `BasicCache` that inherits from `BaseCaching`
+    and is a caching system
+    '''
 
-                       def put(self, key, item):
-                                   '''assign to the dictionary `self.cache_data` the
-                                              `item` value for the key `key`
-                                                      '''
-                                                              if key is not None and item is not None:
-                                                                              self.cache_data[key] = item
+    def put(self, key, item):
+        '''assign to the dictionary `self.cache_data` the
+        `item` value for the key `key`
+        '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
-                                                                                  def get(self, key):
-                                                                                              '''return the value in `self.cache_data` linked to `key`
-                                                                                                      '''
-
-                                                                                                              return self.cache_data.get(key, None)
-
+    def get(self, key):
+        '''return the value in `self.cache_data` linked to `key`
+        '''
+        return self.cache_data.get(key, None)
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -8,34 +8,33 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-        '''A class `LRUCache` that inherits from
-               `BaseCaching` and is a caching system
-                   '''
+    '''A class `LRUCache` that inherits from
+    `BaseCaching` and is a caching system
+    '''
 
-                       def __init__(self):
-                                   '''initialize the cache
-                                           '''
-                                                   super().__init__()
-                                                           self.cache_data = OrderedDict()
+    def __init__(self):
+        '''initialize the cache
+        '''
+        super().__init__()
+        self.cache_data = OrderedDict()
 
-                                                               def put(self, key, item):
-                                                                           """Adds an item in the cache.
-                                                                                   """
-                                                                                           if key is None or item is None:
-                                                                                                           return
-                                                                                                               if key not in self.cache_data:
-                                                                                                                               if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                                                                                                                                                   lru_key, _ = self.cache_data.popitem(True)
-                                                                                                                                                                   print("DISCARD:", lru_key)
-                                                                                                                                                                               self.cache_data[key] = item
-                                                                                                                                                                                           self.cache_data.move_to_end(key, last=False)
-                                                                                                                                                                                                   else:
-                                                                                                                                                                                                                   self.cache_data[key] = item
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
-                                                                                                                                                                                                                       def get(self, key):
-                                                                                                                                                                                                                                   """Retrieves an item by key.
-                                                                                                                                                                                                                                           """
-                                                                                                                                                                                                                                                   if key is not None and key in self.cache_data:
-                                                                                                                                                                                                                                                                   self.cache_data.move_to_end(key, last=False)
-                                                                                                                                                                                                                                                                           return self.cache_data.get(key, None)
-
+    def get(self, key):
+        """Retrieves an item by key
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
